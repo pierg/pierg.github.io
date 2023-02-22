eddd4942ef6ea5af13f77b30b18ad1e04aef4150
pier <piergiuseppe.mallozzi@gmail.com>
2023-02-22 15:39:41 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Switch,
+  Redirect,
+  Route,
+} from 'react-router-dom';
 import Main from './layouts/Main'; // fallback for lazy pages
 import './static/css/main.scss'; // All of our styles
 
@@ -19,6 +24,9 @@ const App = () => (
   <BrowserRouter basename={PUBLIC_URL}>
     <Suspense fallback={<Main />}>
       <Switch>
+        <Route exact path="/cv">
+          <Redirect to={`${process.env.PUBLIC_URL}/cv.pdf`} />
+        </Route>
         <Route exact path="/" component={Index} />
         <Route path="/projects" component={Projects} />
         <Route path="/contact" component={Contact} />
--- src/data/tools.md
+++
@@ -1,6 +0,0 @@
-# Favorite Tools
-
-* Typing: [MyPu](https://github.com/python/mypy)
-* Worked on [_Detect and AvoID Alerting Logic for Unmanned Systems DAIDALUS_](https://github.com/nasa/daidalus) at [NASA Langley Research Center](https://www.nasa.gov/langley).
-* Worked within the [_CHI+MED_](https://www.chi-med.ac.uk/research/) research team to developed analysis tools for medical devices.
-
