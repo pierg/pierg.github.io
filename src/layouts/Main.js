3f83e7f3951bfb56479ec44463e00a8c2302bbab
pier <piergiuseppe.mallozzi@gmail.com>
2023-03-06 18:08:47 -0800

    google

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -2,28 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
-import Analytics from '../components/Template/Analytics';
+// import Analytics from '../components/Template/Analytics';
+import { GTMProvider } from '@elgorditosalsero/react-gtm-hook';
 import Navigation from '../components/Template/Navigation';
 import SideBar from '../components/Template/SideBar';
 import ScrollToTop from '../components/Template/ScrollToTop';
 
-const Main = (props) => (
-  <HelmetProvider>
-    <Analytics />
-    <ScrollToTop />
-    <Helmet titleTemplate="%s | Piergiuseppe Mallozzi" defaultTitle="Piergiuseppe Mallozzi" defer={false}>
-      {props.title && <title>{props.title}</title>}
-      <meta name="description" content={props.description} />
-    </Helmet>
-    <div id="wrapper">
-      <Navigation />
-      <div id="main">
-        {props.children}
-      </div>
-      {props.fullPage ? null : <SideBar />}
-    </div>
-  </HelmetProvider>
-);
+const Main = (props) => {
+  const gtmParams = { id: 'G-GFM7XET0JB' };
+
+  return (
+    <GTMProvider state={gtmParams}>
+      <HelmetProvider>
+        {/* <Analytics /> */}
+        <ScrollToTop />
+        <Helmet
+          titleTemplate="%s | Piergiuseppe Mallozzi"
+          defaultTitle="Piergiuseppe Mallozzi"
+          defer={false}
+        >
+          {props.title && <title>{props.title}</title>}
+          <meta name="description" content={props.description} />
+        </Helmet>
+        <div id="wrapper">
+          <Navigation />
+          <div id="main">{props.children}</div>
+          {props.fullPage ? null : <SideBar />}
+        </div>
+      </HelmetProvider>
+    </GTMProvider>
+  );
+};
 
 Main.propTypes = {
   children: PropTypes.oneOfType([
