040e42d1634ff36a9624d6a00d46286b8eafa0e9
pier <piergiuseppe.mallozzi@gmail.com>
2023-05-03 19:20:47 -0700

    update

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -36,38 +36,69 @@ const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <MePic
-          alt=""
-        />
+        <MePic alt="" />
       </Link>
       <header>
         <h2>Piergiuseppe Mallozzi</h2>
-        <p><a href="mailto:mallozzi@berkeley.edu">mallozzi@berkeley.edu</a></p>
-        <p><a href="mailto:piergiuseppe.mallozzi@gmail.com">piergiuseppe.mallozzi@gmail.com</a></p>
+        <p>
+          <a href="mailto:mallozzi@berkeley.edu">mallozzi@berkeley.edu</a>
+        </p>
+        <p>
+          <a href="mailto:piergiuseppe.mallozzi@gmail.com">
+            piergiuseppe.mallozzi@gmail.com
+          </a>
+        </p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Pier.
-        I am a postdoctoral researcher at <a href="https://www.icyphy.org/people.html/"> in the Industrial Cyber-Physical Systems group at UC Berkeley</a>.
-        I received my PhD in <a href="https://www.chalmers.se/en/Pages/default.aspx">Computer Science from Chalmers University of Technology, Sweden</a> and have been a part of the <a href="https://wasp-sweden.org">Wallenberg AI, Autonomous Systems and Software Program</a>, the largest individual research program in Sweden, since 2016.
-        My background includes a BSc and MSc in Computer Engineering from the <a href="https://www.unipi.it/index.php/english"> University of Pisa, Italy </a>.
-      </p>
       <p>
+        Hi, I&apos;m Pier. I am a postdoctoral researcher at{' '}
+        <a href="https://www.icyphy.org/people.html/">
+          {' '}
+          in the Industrial Cyber-Physical Systems group at UC Berkeley
+        </a>
+        . I received my PhD in{' '}
+        <a href="https://www.chalmers.se/en/Pages/default.aspx">
+          Computer Science from Chalmers University of Technology, Sweden
+        </a>{' '}
+        and have been a part of the{' '}
+        <a href="https://wasp-sweden.org">
+          Wallenberg AI, Autonomous Systems and Software Program
+        </a>
+        , the largest individual research program in Sweden, since 2016. My
+        background includes a BSc and MSc in Computer Engineering from the{' '}
+        <a href="https://www.unipi.it/index.php/english">
+          {' '}
+          University of Pisa, Italy{' '}
+        </a>
+        .
+      </p>
+      {/* <p>
         My research focuses on software engineering, formal methods, and machine learning,
         with a particular interest in design methodologies for autonomous systems.
-      </p>
+      </p> */}
       <ul className="actions">
         <li>
-          {!window.location.pathname.includes('/resume') ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/projects" className="button">About My Projects</Link>}
+          {!window.location.pathname.includes('/resume') ? (
+            <Link to="/resume" className="button">
+              Learn More
+            </Link>
+          ) : (
+            <Link to="/projects" className="button">
+              About My Projects
+            </Link>
+          )}
         </li>
       </ul>
     </section>
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Piergiuseppe Mallozzi <Link to="/">pierg.github.io</Link>.</p>
+      <p className="copyright">
+        &copy; Piergiuseppe Mallozzi <Link to="/">pierg.github.io</Link>.
+      </p>
     </section>
   </section>
 );
