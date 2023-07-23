c0949fab80e820b4a347fa60b0fbd4af6cf5a11b
pier <piergiuseppe.mallozzi@gmail.com>
2023-07-23 12:19:10 -0700

    update

--- src/components/Projects/Cell.js
+++ src/components/Projects/Cell.js
@@ -1,36 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 
-const Cell = ({ data }) => (
-  <div className="cell-container">
-    <article className="mini-post">
-      <header>
-        <h2>{data.title}</h2>
-        <h4>{data.subtitle}</h4>
-      </header>
-      <div className="image">
-        <img src={`${process.env.PUBLIC_URL}${data.image}`} alt={data.title} />
-      </div>
-      <div className="description">
-        <div className="descp">
-          <p> {data.desc} </p>
-          <div className="github-link">
-            <a href={data.github} className="button medium float_right">
-              view of GitHub <FontAwesomeIcon icon={faGithub} />
-            </a>
-            {data.link && (
-              <a href={data.link} className="button medium float_right">
-                Website
+const Cell = ({ data }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
+  return (
+    <div className="cell-container">
+      <article className={`mini-post ${imageLoaded ? 'image-loaded' : ''}`}>
+        <header>
+          <h2>{data.title}</h2>
+          <h4>{data.subtitle}</h4>
+        </header>
+        <div className="image">
+          {!imageLoaded && <div className="loading-placeholder">Loading...</div>}
+          <img
+            src={`${process.env.PUBLIC_URL}${data.image}`}
+            alt={data.title}
+            onLoad={handleImageLoad}
+            style={{ display: imageLoaded ? 'block' : 'none' }}
+          />
+        </div>
+        <div className="description">
+          <div className="descp">
+            <p> {data.desc} </p>
+            <div className="github-link">
+              <a href={data.github} className="button medium float_right">
+                view on GitHub <FontAwesomeIcon icon={faGithub} />
               </a>
-            )}
+              {data.link && (
+                <a href={data.link} className="button medium float_right">
+                  Website
+                </a>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </article>
-  </div>
-);
+      </article>
+    </div>
+  );
+};
 
 Cell.propTypes = {
   data: PropTypes.shape({
